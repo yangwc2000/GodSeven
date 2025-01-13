0f8d9b695a0f073ca4b5c5c491d426b78e616b18
yangwc2000 <yangwoochang@naver.com>
2025-01-13 22:54:18 +0900

    GodSeven
    Team Project

---
+++ Item.cpp
@@ -0,0 +1,32 @@
+﻿//item.cpp
+#include "Item.h"
+#include "Character.h"
+#include <iostream>
+
+HealthPotion::HealthPotion() : healthRestore(50) {}
+
+std::string HealthPotion::getName() const {
+    return "Health Potion";
+}
+
+void HealthPotion::use(Character* character) {
+    if (character) {
+        std::cout << getName() << " 을(를) 사용합니다!\n";
+        // 실제로 체력 회복 로직을 호출
+        character->recoverHealth(healthRestore);
+    }
+}
+
+AttackBoost::AttackBoost() : attackIncrease(10) {}
+
+std::string AttackBoost::getName() const {
+    return "Attack Boost";
+}
+
+void AttackBoost::use(Character* character) {
+    if (character) {
+        std::cout << getName() << " 을(를) 사용합니다!\n";
+        // 실제로 공격력 증가 로직을 호출
+        character->increaseAttack(attackIncrease);
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿//main.cpp
+#include "GameManager.h"
+
+int main() {
+    GameManager gameManager;
+    gameManager.startGame();
+    return 0;
+}
